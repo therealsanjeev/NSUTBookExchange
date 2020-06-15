78c31845527841292c774e53f3c5d0ebb4f80468
Sanjeev <therealsanjeev0@gmail.com>
2020-06-16 03:19:40 +0530

    under progress

---
+++ app/src/main/java/com/therealsanjeev/nsutbookexchange/homepage.java
@@ -0,0 +1,14 @@
+package com.therealsanjeev.nsutbookexchange;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
