831cc10a3b463652d46c09e6a58ff0c292e1e38e
Sanjeev <therealsanjeev0@gmail.com>
2020-06-16 19:17:29 +0530

    book Details

---
+++ app/src/main/java/com/therealsanjeev/nsutbookexchange/buy/BookDetails.java
@@ -0,0 +1,16 @@
+package com.therealsanjeev.nsutbookexchange.buy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.therealsanjeev.nsutbookexchange.R;
+
+public class BookDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_details);
+    }
+}
