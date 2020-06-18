7a0266136bac3926574bd159a5719226c1dab24b
Sanjeev <therealsanjeev0@gmail.com>
2020-06-18 09:20:41 +0530

    Update bookDetails

--- app/src/main/java/com/therealsanjeev/nsutbookexchange/buy/BookDetails.java
+++ app/src/main/java/com/therealsanjeev/nsutbookexchange/buy/BookDetails.java
@@ -3,7 +3,10 @@ package com.therealsanjeev.nsutbookexchange.buy;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.therealsanjeev.nsutbookexchange.R;
@@ -12,12 +15,22 @@ public class BookDetails extends AppCompatActivity {
 
     Toolbar toolbar;
     TextView bookName,authName,price,name,number,email;
+    String phoneNo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_details);
         setType();
         getData();
+
+        number.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(phoneNo));
+                startActivity(intent);
+            }
+        });
     }
 
     private void getData() {
@@ -26,7 +39,8 @@ public class BookDetails extends AppCompatActivity {
             bookName.setText((String) bundle.get("book"));
             authName.setText((String)bundle.get("auth"));
             price.setText("₹"+(String)bundle.get("price"));
-            name.setText((String)bundle.get("seller"));
+            phoneNo=(String)bundle.get("seller");
+            name.setText(phoneNo);
             number.setText((String)bundle.get("number"));
             email.setText((String)bundle.get("email"));
         }
