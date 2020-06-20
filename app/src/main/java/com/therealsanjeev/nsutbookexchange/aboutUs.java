419cf0b5214eaccb44e6e952f18b2cbc36e1a938
Sanjeev Gupta <sanjeev.mp19@nsut.ac.in>
2020-06-21 03:42:29 +0530

    Update about

--- app/src/main/java/com/therealsanjeev/nsutbookexchange/aboutUs.java
+++ app/src/main/java/com/therealsanjeev/nsutbookexchange/aboutUs.java
@@ -3,9 +3,16 @@ package com.therealsanjeev.nsutbookexchange;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class aboutUs extends AppCompatActivity {
+
+    Button button;
     Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +23,23 @@ public class aboutUs extends AppCompatActivity {
         toolbar.setTitle("About");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        button=findViewById(R.id.aboutBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://instagram.com/therealsanjeev");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://instagram.com/therealsanjeev")));
+                }
+            }
+        });
     }
 }
